6ccd87e9d384410adf97133af50a050c6f34b781
gashman878 <gashman878@gmail.com>
2025-07-09 00:53:49 +0100

    onGod

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import logo from '../assets/images/logo.png';
 import { NavLink } from 'react-router-dom';
-
 
 
 const style = {backgroundColor: "black"};
 const nostyle = {backgroundColor: ""};
 const activeLink = ({isActive}) => isActive ? style : nostyle ;
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log('You Clicked the menu');
+  };
   return (
-    <div className="navbar">
+    <>
+      <div className="navbar">
         <div className="navholder">
           <div className="logoHolder">
             <img className="logo" src={ logo } />
@@ -20,12 +26,35 @@ const Navbar = () => {
             <li><NavLink className="links" to='/jobs' style={ activeLink}>Jobs</NavLink></li>
             <li><NavLink className="links" to='/add-job' style={ activeLink }>Add Job</NavLink></li>
           </ul>
+
           <div className='menu'>
-            <button className='menuBtn'>Menu</button>
+            {/* Mobile Menu Button */}
+            <button className='menuBtn' onClick={toggleMenu} aria-label='Toggle menu'>{isMenuOpen ? '✕' : 'Menu'}</button>
+            
           </div>
         </div>
       </div>
-  )
-}
+
+
+      {/* Mobile Off-Canvas Menu */}
+      <div className={`offcanvas ${isMenuOpen ? 'open' : ''}`}>
+        <div className="offcanvas-header">
+          <img className="logo" src={logo} alt="Jobs Board Logo" />
+          <p className="logoCaption">Jobs Board</p>
+        </div>
+        <ul className="mobile-navLinks">
+          <li><NavLink className="links" to='/' style={activeLink} onClick={toggleMenu}>Home</NavLink></li>
+          <li><NavLink className="links" to='/jobs' style={activeLink} onClick={toggleMenu}>Jobs</NavLink></li>
+          <li><NavLink className="links" to='/add-job' style={activeLink} onClick={toggleMenu}>Add Job</NavLink></li>
+        </ul>
+      </div>
+
+      {/* Overlay */}
+      {isMenuOpen && <div className="overlay" onClick={toggleMenu} />}
+    
+    </>
+
+  );
+};
 
 export default Navbar
