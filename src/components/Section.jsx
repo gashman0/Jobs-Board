d3baf6c07e54263e2e540b08df2a4ebd8dc5962e
gashman0 <gashman878@gmail.com>
2025-09-13 11:14:25 +0100

    bug fixes

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,17 +8,15 @@ const Section = () => {
   return (
     <section>
         <div className="forOption">
-          <div>
-            <div className="employers">
-                <p className="devEmp">For Developers</p>
-                <p className="devCaption">Browse our jobs/skills and start your career today for a better tomorrow.</p>
-                <Link className='browse' to='/jobs'>Browse Jobs</Link>
-            </div>
-            <div className="employers last">
-                <p className="devEmp">For Employers</p>
-                <p className="devCaption">List your job to find the perfect developer/employee for the role.</p>
-                <Link className='add' to='/add-job'>Add Job</Link>
-            </div>
+          <div className="employers">
+              <p className="devEmp">For Developers</p>
+              <p className="devCaption">Browse our jobs/skills and start your career today for a better tomorrow.</p>
+              <Link className='browse' to='/jobs'>Browse Jobs</Link>
+          </div>
+          <div className="employers">
+              <p className="devEmp">For Employers</p>
+              <p className="devCaption">List your job to find the perfect developer/employee for the role.</p>
+              <Link className='add' to='/add-job'>Add Job</Link>
           </div>
         </div>
     </section>
